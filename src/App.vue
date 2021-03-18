4f3f16ac1a11dc68fa60c4d624697a45201e5d8a
MuhammadSalman009-tech <salmansumra009@gmail.com>
2021-03-18 20:24:03 +0500

    two way data binding in forms

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
+<SignUpForm/>
 </template>
 
 <script>
+import SignUpForm from "./components/SignUpForm"
 export default {
   name: 'App',
   components: {
+    SignUpForm
   }
 }
 </script>
@@ -17,5 +20,42 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  margin: 0;
+  background-color: #eee;
+}
+form{
+  max-width: 420px;
+  margin: 30px auto;
+  background-color: #fff;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label{
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0px 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input, select{
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+input[type="checkbox"]{
+  display: inline-block;
+  width: 16px;
+  margin: 0px 10px 0px 0px;
+  position: relative;
+  top: 2px;
 }
 </style>
---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <form action="">
+    <label for="email">Email</label>
+    <input type="email" v-model="email" required>
+
+    <label for="password">Password</label>
+    <input type="password" v-model="password" required>
+
+    <label for="role">Role</label>
+    <select name="" id="" v-model="role" required>
+        <option value="developer">Web developer</option>
+        <option value="designer">Web designer</option>
+    </select>
+
+    <input type="checkbox" v-model="terms" required>
+    <label for="terms">I accept the terms and conditions</label>
+  </form>
+  <p>Email: {{email}}</p>
+  <p>Password: {{password}}</p>
+  <p>Role: {{role}}</p>
+  <p>Terms: {{terms}}</p>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email:'',
+            password:'',
+            role:'',
+            terms:false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
